62b86d7dd11f91ab48233093a4a2b71eb9ac69f1
Oleg Zheleznov <oleg-zheleznov@mail.ru>
2022-06-15 19:22:44 +0300

    Fix test

--- src/main/java/school/mjc/stage0/loops/task4/FixMe.java
+++ src/main/java/school/mjc/stage0/loops/task4/FixMe.java
@@ -3,7 +3,13 @@ package school.mjc.stage0.loops.task4;
 public class FixMe {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-
+            if (i % 2 == 0) {
+                System.out.println(i);
+            } else if (i == 7) {
+                System.out.println("last");
+                break;
+                System.out.println("will I be printed?");
+            }
         }
     }
 }
